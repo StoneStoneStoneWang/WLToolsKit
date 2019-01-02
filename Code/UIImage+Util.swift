2e54623eabf9cc0594ca53ed8587389fc918b3f0
StoneStoneStoneWang <yuanxingfu1314@163.com>
2019-01-02 10:32:46 +0800

    1.1.9添加图片压缩2分法

--- Code/UIImage+Util.swift
+++ Code/UIImage+Util.swift
@@ -104,4 +104,97 @@ extension UIImage {
         return newImage
     }
 }
+extension UIImage {
+    
+    // MARK: 存入图片
+    public static func wl_imagePath(image: UIImage,imageName: String) -> String {
+        
+        let data = image.jpegData(compressionQuality: 1)
+        
+        let documentsPath = (NSHomeDirectory() as NSString).appendingPathComponent("Documents")
+        
+        let fm = FileManager.default
+        
+        try! fm.createDirectory(atPath: documentsPath, withIntermediateDirectories:  true, attributes: nil)
+        
+        let imagePath = "/\(imageName)"
+        
+        fm.createFile(atPath: "\(documentsPath)\(imagePath)", contents: data, attributes: nil)
+        
+        return "\(documentsPath)\(imagePath)"
+    }
+    
+    // MARK: 存入图片
+    public static func wl_imagePath(data: Data,imageName: String) -> String {
+        
+        let documentsPath = (NSHomeDirectory() as NSString).appendingPathComponent("Documents")
+        
+        let fm = FileManager.default
+        
+        try! fm.createDirectory(atPath: documentsPath, withIntermediateDirectories:  true, attributes: nil)
+        
+        let imagePath = "/\(imageName)"
+        
+        fm.createFile(atPath: "\(documentsPath)\(imagePath)", contents: data, attributes: nil)
+        
+        return "\(documentsPath)\(imagePath)"
+    }
+    
+    // MARK: 移除图片
+    public static func wl_removeImageAtPath(imageName: String) {
+        let documentsPath = (NSHomeDirectory() as NSString).appendingPathComponent("Documents")
+        
+        let fm = FileManager.default
+        
+        let filePath = "\(documentsPath)/\(imageName)"
+        
+        do {
+            
+            try fm.removeItem(atPath: filePath)
+            
+        } catch {
+            
+            printLog(message: error)
+        }
+    }
+    
+}
+
+extension UIImage {
+    
+    // MARK: 二分法 压缩图片
+    public static func compressImage(_ image: UIImage ,maxLength: NSInteger) -> Data {
+        
+        var compression: CGFloat = 1
+        
+        var data = image.jpegData(compressionQuality: compression)!
+        
+        if data.count < maxLength { return data}
+        
+        var max: CGFloat = 1
+        
+        var min: CGFloat = 0
+        
+        for _ in 0..<6 {
+            
+            compression = (max + min) / 2
+            
+            data = image.jpegData(compressionQuality: compression)!
+            
+            if data.count < NSInteger(Double(maxLength) * 0.9) {
+                
+                min = compression
+            } else if data.count > maxLength {
+                
+                max = compression
+            } else {
+                
+                break
+            }
+        }
+        
+        return data
+    }
+}
+
 
